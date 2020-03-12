0c271702318b78a416d16be51667bceb302ce611
tail <vclp@163.com>
2020-03-12 13:04:37 +0800

    feat✨  redis 操作

--- src/cache/_redis.js
+++ src/cache/_redis.js
@@ -14,5 +14,46 @@ redisClient.on("error", err => {
 });
 
 // set
-
+/**
+ *
+ * @param {string} key 键
+ * @param {string} val 值
+ * @param {number} timeout 过期时间，单位 s
+ */
+function set(key, val, timeout = 60 * 60) {
+  if (typeof val === "object") {
+    val = JSON.stringify(val);
+  }
+  redisClient.set(key, val);
+  redisClient.expire(key, timeout);
+}
 // get
+
+/**
+ *
+ * @param {string} key 键
+ */
+function get(key) {
+  const promise = new Promise((reslove, reject) => {
+    redisClient.get(key, (err, val) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      if (val == null) {
+        reslove(null);
+        return;
+      }
+      try {
+        reslove(JSON.parse(val));
+      } catch (ex) {
+        reslove(val);
+      }
+    });
+  });
+  return promise;
+}
+
+module.exports = {
+  set
+};
